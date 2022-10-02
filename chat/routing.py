6ec6b9610e16f0324720db1e1a59fb58c250b6da
Siddharth <f20210438@pilani.bits-pilani.ac.in>
2022-10-02 11:03:20 +0530

    check

--- chat/routing.py
+++ chat/routing.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/chatroom/collection/',consumers.ChatroomStatus.as_asgi()),
-    path('ws/chatroom/detail/<str:chatroom_id>/',consumers.ChatroomDetailStatus.as_asgi()),
+    path('ws/chatroom/collection/',consumers.ChatroomStatus),
+    path('ws/chatroom/detail/<str:chatroom_id>/',consumers.ChatroomDetailStatus),
 ]
